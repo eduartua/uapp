bd29025f95a045e3e2fd3bbc490362412308e1e7
eduartua <eduartua@gmail.com>
2019-04-21 23:22:24 -0500

    changed path in view

--- views/view.go
+++ views/view.go
@@ -8,8 +8,8 @@ import (
 // NewView generates a new view
 func NewView(layout string, files ...string) *View {
 	files = append(files,
-		"views/layouts/footer.gohtml",
-		"views/layouts/bootstrap.gohtml")
+		"/test/uapp/views/layouts/footer.gohtml",
+		"/test/uapp/views/layouts/bootstrap.gohtml")
 	t, err := template.ParseFiles(files...)
 	if err != nil {
 		panic(err)
